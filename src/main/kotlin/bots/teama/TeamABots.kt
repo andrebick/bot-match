24fa7767f5f99519ef6456dba18136a10ac913a0
Praktikum Team C <schuelerpraktikumcc+c@gmail.com>
2026-07-06 11:54:24 +0200

    SkibidiTerminator Story 1 und 2

--- src/main/kotlin/bots/teama/TeamABots.kt
+++ src/main/kotlin/bots/teama/TeamABots.kt
@@ -10,12 +10,51 @@ import framework.arena.RobotBrain
 import framework.arena.Sensors
 
 /** Startpunkt für euren eigenen Bot - benennt/erweitert diese Klasse nach Belieben. */
-class MeinBot(override val name: String = "Team A - MeinBot") : RobotBrain {
+class MeinBot(override val name: String = "Team A - SkibidiTerminator") : RobotBrain {
     override fun decide(sensors: Sensors): Action {
-        // TODO: Bewege dich stattdessen in Richtung des nächsten Gegners
-        // TODO: Schieße wenn ein Gegner in Sichtlinie ist
-        // TODO: Reagiere auf niedrige eigene HP (sensors.self.health), z.B. mit Flucht
-        return Action.Move(Direction.SOUTH)
+        var direction = Direction.entries.random()
+        println(direction)
+        println(sensors)
+        println (sensors.self.position)
+
+
+
+        val x = sensors.self.position.x
+        val y = sensors.self.position.y
+
+        if (x < 5) {
+            direction = Direction.WEST
+        }
+        else{
+            direction = Direction.EAST
+        }
+
+        if (y < 5) {
+            direction = Direction.NORTH
+        }
+        else{
+            direction = Direction.SOUTH
+        }
+
+        if (x == 0 || x == 9){
+            if (y < 5){
+                direction = Direction.NORTH
+            }
+            else{
+                direction = Direction.SOUTH
+            }
+        }
+
+        if (y == 0 || y == 9){
+            if (x < 5){
+                direction = Direction.WEST
+            }
+            else{
+                direction = Direction.EAST
+            }
+        }
+
+        return Action.Move(direction)
     }
 }
 
